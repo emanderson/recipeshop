bdb9ad9dc1f74e55fa81817ea0e48f470d8badf4
Emily Anderson <emily@eandersons.net>
2014-06-05 10:37:25 -0700

    Add basic "Hello World" server

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = flag.String("port", "8081", "port to serve on")
+
+func RecipeShopServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello again, world!")
+}
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/", RecipeShopServer)
+
+	err := http.ListenAndServe(":" + *port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
